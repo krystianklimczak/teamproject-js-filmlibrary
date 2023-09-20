c0fa6937d907ebf1c7e633b6a6c627b614615ba9
krystianklimczak <krystianklimczak1996r.com>
2023-09-20 20:10:40 +0200

    update axios second method fetching data from TMDB

--- src/js/filmApi.js
+++ src/js/filmApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/movie/popular';
+// FIRST METHOD
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
 axios.defaults.params = {
   api_key: '95f474a01cc4252905d63c7d958d5749',
 };
@@ -9,3 +10,22 @@ export const responseAxios = axios
   .get()
   .then(responseA => console.log(responseA.data.results))
   .catch(error => console.log(error));
+
+// SECOND METHOD
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer 95f474a01cc4252905d63c7d958d5749',
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
