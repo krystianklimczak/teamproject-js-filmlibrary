d57bde31fe649f8c222a18d915f9258a307a547d
krystianklimczak <krystianklimczak1996r.com>
2023-09-23 22:47:37 +0200

    test

---
+++ src/my-library.js
@@ -0,0 +1,17 @@
+import './sass/main.scss';
+
+const navigation = document.querySelectorAll('.navigation__item');
+
+for (const item of navigation) {
+  item.classList.toggle('is-active');
+}
+
+const library = document.querySelector('.my-library');
+
+if (library.innerHTML === '') {
+  library.classList.add('is-empty');
+}
+
+const header = document.querySelector('.header');
+
+header.classList.toggle('header-library');
