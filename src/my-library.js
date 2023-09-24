2b9ba60dd20107407e7b6dca57775d40d920d621
krystianklimczak <krystianklimczak1996r.com>
2023-09-24 11:07:42 +0200

    test

--- src/my-library.js
+++ src/my-library.js
@@ -1,20 +1,32 @@
 import './sass/main.scss';
 
 const navigation = document.querySelectorAll('.navigation__item');
+const myLibrary = document.querySelector('.my-library');
+const header = document.querySelector('.header');
+const form = document.querySelector('.search-form');
+const libraryBtns = document.querySelectorAll('.library__button');
+const watched = document.querySelector('.watched');
+const queue = document.querySelector('.queue');
 
 for (const item of navigation) {
   item.classList.toggle('is-active');
 }
 
-const library = document.querySelector('.my-library');
-
-if (library.innerHTML === '') {
-  library.classList.add('is-empty');
+if (myLibrary.innerHTML === '') {
+  myLibrary.classList.add('is-empty');
 }
-
-const header = document.querySelector('.header');
 
 header.classList.toggle('header-library');
 
-const form = document.querySelector('.search-form');
 form.classList.add('is-hidden');
+
+for (const btn of libraryBtns) {
+  btn.classList.toggle('is-hidden');
+}
+
+for (const btn of libraryBtns) {
+  btn.addEventListener('click', e => {
+    const classArray = e.target.classList;
+    btn.classList.toggle('library__button--current');
+  });
+}
