5256a3dd43cdea2982cad2fa33d2f0a3a4b20d79
krystianklimczak <krystianklimczak1996r.com>
2023-09-24 21:39:42 +0200

    test

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -12,7 +12,7 @@ const modalBox = document.querySelector('.modal-trailer');
 
 function openModalByKey(key) {
   modalBox.innerHTML = '';
-  const text = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${key}?si=p33dKxNf4oNZFFq6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+  const text = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
   modalBox.insertAdjacentHTML('beforeend', text);
   modalBox.classList.remove('is-hidden');
   window.addEventListener('keydown', escape);
