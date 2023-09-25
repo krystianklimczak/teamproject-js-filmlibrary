775f752fd38e2fd8fc5da2825b361f2a854b619c
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-09-25 20:34:27 +0200

    styles

---
+++ src/my_library.js
@@ -0,0 +1,35 @@
+import './sass/main.scss';
+
+const navigation = document.querySelectorAll('.navigation__item');
+const myLibrary = document.querySelector('.my-library');
+const header = document.querySelector('.header');
+const form = document.querySelector('.search-form');
+const libraryBtns = document.querySelectorAll('.library__button');
+const watched = document.querySelector('.watched');
+const queue = document.querySelector('.queue');
+
+for (const item of navigation) {
+  item.classList.toggle('is-active');
+}
+
+if (myLibrary.innerHTML === '') {
+  myLibrary.classList.add('is-empty');
+}
+
+header.classList.toggle('header-library');
+
+form.classList.add('is-hidden');
+
+for (const btn of libraryBtns) {
+  btn.classList.toggle('is-hidden');
+  btn.addEventListener('click', e => {
+    const arrayL = [...e.target.classList];
+    const check = arrayL.some(el => el === 'library__button--current');
+    if (check) {
+      return;
+    } else {
+      watched.classList.toggle('library__button--current');
+      queue.classList.toggle('library__button--current');
+    }
+  });
+}
