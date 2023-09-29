57b5c00016afb51e62db61e5a60634baee0464f2
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-29 12:15:26 +0200

    modal-film.js / refs.js
    function onModalOpenMovie & object with references

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,10 +1,27 @@
+// import { WATCHED_KEY, QUEUE_KEY } from '.localStorageKeys'; // how should I find this? // do we have localStorage?
 import { refs } from './refs';
+
+refs.filmList.addEventListener('click', event => {
+  onModalOpenMovie(event);
+});
 
 export let id;
 export let movieData = [];
 export function onModalOpenMovie(e) {
   e.preventDefaut();
-  clearModal;
+  clearModal();
+
+  id = Number(e.target.closest('div').dataset.id);
+
+  const videoListWatched = storage.load(WATCHED_KEY) ? storage.load(WATCHED_KEY) : [];
+  //   const videoListQueue = storage.load(QUEUE_KEY) ? storage.load(QUEUE_KEY) : [];
+
+  refs.toWatchBtn.textContent = buttonText(
+    videoListWatched,
+    id,
+    'Add to watched',
+    'Remove from watched',
+  );
 }
 
 function clearModal() {
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,5 @@
 export const refs = {
+  filmList: document.querySelector('.main-section'),
   modalCard: document.querySelector('.modal-film__card'),
+  toWatchBtn: document.querySelector('.modal-film__btns-addToWatched'),
 };
