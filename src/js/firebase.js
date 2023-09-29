a64afa4754162306e96f14c87c75abebbfa18942
DamianKaczmarczyk <jokernick@o2.pl>
2023-09-29 09:13:17 +0200

    firebase installed + Config
    firebase installed + Config

---
+++ src/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbg53o9UCxSN9_wbNkFiy_0TNxnZAP13Y",
+  authDomain: "teamproject-js-filmlibrary.firebaseapp.com",
+  projectId: "teamproject-js-filmlibrary",
+  storageBucket: "teamproject-js-filmlibrary.appspot.com",
+  messagingSenderId: "796295773410",
+  appId: "1:796295773410:web:30f3bfb2b903260d1832bb",
+  measurementId: "G-GBHFN6HXPV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
