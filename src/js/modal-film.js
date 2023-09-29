387e4d1a65a8567dac391bc028bd49039e7abdcb
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-29 12:40:34 +0200

    modal-film.js / refs.js / _modal-film.scss
    Function closeModal() & onModalOpenMovie() changes.
    Minor changes to *.scss & new references.

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -13,15 +13,33 @@ export function onModalOpenMovie(e) {
 
   id = Number(e.target.closest('div').dataset.id);
 
-  const videoListWatched = storage.load(WATCHED_KEY) ? storage.load(WATCHED_KEY) : [];
-  //   const videoListQueue = storage.load(QUEUE_KEY) ? storage.load(QUEUE_KEY) : [];
+  const moviesListWatched = storage.load(WATCHED_KEY) ? storage.load(WATCHED_KEY) : [];
+  const moviesListQueue = storage.load(QUEUE_KEY) ? storage.load(QUEUE_KEY) : [];
 
   refs.toWatchBtn.textContent = buttonText(
-    videoListWatched,
+    moviesListWatched,
     id,
     'Add to watched',
     'Remove from watched',
   );
+
+  refs.toQueueBtn.textContent = buttonText(
+    moviesListQueue,
+    id,
+    'Add to queue',
+    'Remove from queue',
+  );
+
+  refs.modalBackdrop.classList.add('active');
+  refs.modalFilm.classList.add('active');
+  document.body.classList.add('modal-is-hidden');
+  refs.modalFilmBtnClose.addEventListener('click', closeModal);
+}
+
+export function closeModal() {
+  refs.modalBackdrop.classList.remove('active');
+  refs.modalFilm.classList.remove('active');
+  document.body.classList.remove('modal-is-hidden');
 }
 
 function clearModal() {
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,7 @@ export const refs = {
   filmList: document.querySelector('.main-section'),
   modalCard: document.querySelector('.modal-film__card'),
   toWatchBtn: document.querySelector('.modal-film__btns-addToWatched'),
+  modalBackdrop: document.querySelector('.backdrop-modal-film'),
+  modalFilm: document.querySelector('.modal-film'),
+  modalFilmBtnClose: document.querySelector('.modal-film__btn-close'),
 };
