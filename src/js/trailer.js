1b8c76eb30223c17a2847e019bdd9deae6788937
MartaKoss27 <27691497m@gmail.com>
2023-09-30 12:12:42 +0200

    update bugfix/trailer-styles
     trailer frame and close button-draft- to be changed

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -12,30 +12,38 @@ export async function getTrailerKey(id) {
   }
 }
 const backdrop = document.querySelector('.modal-backdrop');
+const trailer = document.querySelector('.modal-trailer');
 
 export function showTrailer(key) {
   // `<iframe width="560" height="315" src="https://www.youtube.com/embed/${key}?si=NP4x3PPUZd7lNCFY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
   if (!key) {
     return Notiflix.Notify.failure('Ups! No video has been found.');
   }
-  const trailer = document.createElement('div');
-  trailer.classList.add('modal-trailer');
+  //const trailer = document.createElement('div');
+  //trailer.classList.add('modal-trailer');
+  trailer.innerHTML = '';
   trailer.insertAdjacentHTML(
     'afterbegin',
     `<iframe class="modal-trailer__iframe" src="https://www.youtube.com/embed/${key}?si=NP4x3PPUZd7lNCFY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
   );
+  //TRAILER CLOSE BTN
+  const trailerCloseBtn = `<button class ="modal-trailer__button">X</button>`;
+  trailer.insertAdjacentHTML('beforeend', trailerCloseBtn);
+
+  //
   backdrop.innerHTML = '';
   backdrop.insertAdjacentElement('afterbegin', trailer);
   backdrop.classList.remove('is-hidden');
   document.body.style.overflow = 'hidden';
-  backdrop.addEventListener('click', () => {
-    backdrop.classList.add('is-hidden');
-    backdrop.innerHTML = '';
-    document.body.style.overflow = 'auto';
-  });
+  backdrop.addEventListener('click', closeTrailer);
   document.addEventListener('keydown', handler);
 }
-
+function closeTrailer() {
+  backdrop.classList.add('is-hidden');
+  backdrop.innerHTML = '';
+  document.removeEventListener('click', closeTrailer);
+  document.body.style.overflow = 'auto';
+}
 function handler(e) {
   if (e.key === 'Escape') {
     backdrop.classList.add('is-hidden');
