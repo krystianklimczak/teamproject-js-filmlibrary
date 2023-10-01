0673facd005cdabca7b3b4743e36daebad1c11aa
Sebastian Lalek <123250026+SebastianLalek@users.noreply.github.com>
2023-10-01 19:13:52 +0200

    add visibility switcher

--- src/js/arrow-up.js
+++ src/js/arrow-up.js
@@ -1,4 +1,5 @@
 const arrow = document.querySelector('.arrow');
+const header = document.querySelector('.header');
 
 arrow.addEventListener('click', () => {
   window.scroll({
@@ -6,3 +7,22 @@ arrow.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
+};
+
+let observer = new IntersectionObserver(showArrow, options);
+observer.observe(header);
+
+function showArrow(entries) {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      arrow.classList.add('is-hidden');
+    } else {
+      arrow.classList.remove('is-hidden');
+    }
+  });
+}
