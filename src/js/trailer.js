54819db869b5ca8a74a61cfa722cb0ba9f1c20b6
MartaKoss27 <27691497m@gmail.com>
2023-10-02 13:27:33 +0200

    update bugfix/modal-trailer
    I cleaned the code (removed unnecessary comments etc. nothing major)

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -11,17 +11,14 @@ export async function getTrailerKey(id) {
     return Notiflix.Notify.failure('Ups! No video has been found.');
   }
 }
+
 const backdrop = document.querySelector('.modal-backdrop');
 const trailer = document.querySelector('.modal-trailer');
 
 export function showTrailer(key) {
-  // `<iframe width="560" height="315" src="https://www.youtube.com/embed/${key}?si=NP4x3PPUZd7lNCFY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
   if (!key) {
     return;
   }
-
-  //const trailer = document.createElement('div');
-  //trailer.classList.add('modal-trailer');
   trailer.innerHTML = '';
   trailer.insertAdjacentHTML(
     'afterbegin',
@@ -34,7 +31,6 @@ export function showTrailer(key) {
   const trailerCloseBtn = document.querySelector('.modal-trailer__button');
   trailerCloseBtn.addEventListener('click', closeTrailer);
 
-  //
   backdrop.innerHTML = '';
   backdrop.classList.remove('is-hidden');
   trailer.classList.remove('is-hidden');
