897c6974eaf94dc1fa9243b1eab77d98ae2ead1b
DamianKaczmarczyk <jokernick@o2.pl>
2023-10-02 11:03:02 +0200

    sign modal
    sign modal

--- src/js/modal-signup.js
+++ src/js/modal-signup.js
@@ -1,15 +1,14 @@
-// const openEls = document.querySelectorAll("[data-open]");
-// const isVisible = "is-visible";
-// for(const el of openEls) {
-//   el.addEventListener("click", function() {
-//     const modalId = this.dataset.open;
-//     document.getElementById(modalId).classList.add(isVisible);
-//   });
-// }
-// const closeEls = document.querySelectorAll("[data-close]");
-// const isVisible = "is-visible";
-// for (const el of closeEls) {
-//   el.addEventListener("click", function() {
-//     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-//   });
-// }
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[sign-open]'),
+      closeModalBtn: document.querySelector('[sign-close]'),
+      modal: document.querySelector('[sign-container]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
