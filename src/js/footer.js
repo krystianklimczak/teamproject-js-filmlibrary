c47ac83b2d6b5e1f4525b033ae64520fa3c780a2
Sebastian Lalek <chemiczq@gmail.com>
2023-10-11 21:49:03 +0200

    add media query

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,14 +1,17 @@
 const footer = document.querySelector('.footer');
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 let prevScrollPos = window.scrollY.valueOf();
 
-window.onscroll = () => {
-  const currentScrollPos = window.scrollY.valueOf();
+if (mediaQuery.matches) {
+  window.onscroll = () => {
+    const currentScrollPos = window.scrollY.valueOf();
 
-  if (prevScrollPos > currentScrollPos) {
-    footer.style.bottom = '0';
-  } else {
-    footer.style.bottom = '-50px';
-  }
-  prevScrollPos = currentScrollPos;
-};
+    if (prevScrollPos > currentScrollPos) {
+      footer.style.bottom = '0';
+    } else {
+      footer.style.bottom = '-50px';
+    }
+    prevScrollPos = currentScrollPos;
+  };
+}
