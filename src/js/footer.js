ecf6f3561b88c726b26023151f7886a418ac7882
Sebastian Lalek <chemiczq@gmail.com>
2023-10-13 11:57:39 +0200

    add event listener

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,15 +3,19 @@ const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 let prevScrollPos = window.scrollY.valueOf();
 
-if (mediaQuery.matches) {
-  window.onscroll = () => {
-    const currentScrollPos = window.scrollY.valueOf();
+function addScrollEvent(e) {
+  if (e.matches) {
+    window.onscroll = () => {
+      const currentScrollPos = window.scrollY.valueOf();
 
-    if (prevScrollPos > currentScrollPos) {
-      footer.style.bottom = '0';
-    } else {
-      footer.style.bottom = '-50px';
-    }
-    prevScrollPos = currentScrollPos;
-  };
+      if (prevScrollPos > currentScrollPos) {
+        footer.style.bottom = '0';
+      } else {
+        footer.style.bottom = '-50px';
+      }
+      prevScrollPos = currentScrollPos;
+    };
+  }
 }
+
+mediaQuery.addEventListener('change', addScrollEvent);
